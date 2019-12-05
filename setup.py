3e9ba3bc0b45546aaae372592e55db03370065e0
4NONYM0U5 H4CK3R <37062817+Akashgiri963@users.noreply.github.com>
2019-12-05 08:58:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ import os,sys
 os.chdir('/data/data/com.termux/files/usr/etc')
 
 opr = open('bash.bashrc','a')
-opr.write("\nalias usr='python /data/data/com.termux/files/home/Termux-Lock/Termux-Lock.py -l'\n")
-opr.write('figlet -c -k -f slant Termux-Lock|lolcat;usr\n')
+opr.write("\nalias txl='python /data/data/com.termux/files/home/Termux-Lock/Termux-Lock.py -l'\n")
+opr.write('txl\n')
 opr.close()
 
 os.system('apt install figlet')
 os.system('pip install stdiomask')
+os.system('pip install lolcat')
